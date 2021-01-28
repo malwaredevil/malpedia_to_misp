d9047bd79384af1c92dca999c5825600e1ca90e1
Jonathan Batchelor <jonathan.batchelor@sil.lab>
2021-01-27 18:02:35 -0600

    202101271802

--- initialize.py
+++ initialize.py
@@ -559,6 +559,7 @@ def build_actor_malware_tree(threat_actor):
 def stageActorMalwareMeta():
     # BEGIN DOWNLOADING ALL ACTORS
     mpClient = malpediaclient.Client()
+    mpClient.authenticate(username=gv._MALPEDIA_USER, password=gv._MALPEDIA_PASSWORD)
     mpClient.authenticate_by_token(apitoken=gv._MALPEDIA_KEY)
     gv._ACTORS_LIST = mpClient.list_actors()
     print("f(x) stageActorMalwareMeta: RETRIEVED LIST OF THREAT ACTORS FROM MALPEDIA")
