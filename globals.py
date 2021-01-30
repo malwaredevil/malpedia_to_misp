ec2e29ff230638bd9e66c7460f168d38ae164bfc
Jonathan Batchelor <jonathan.batchelor@sil.lab>
2021-01-30 15:43:12 -0600

    202101301543

--- globals.py
+++ globals.py
@@ -120,7 +120,8 @@ _BLACKLISTED_FILES = [
         'e148c5fbc84930ea84eb6faf1dc19d9b594eedb53276c1be2ec3f5d9847187bc',
         'b4fcc933ce58349063693f4415343ff5eb640bef418b38dda2714f5c34c538f_unpacked',
         'f98bcde75347ea723f0b0f70cce6dfe75525d1be9ca776a868da9402f2e06aff_unpacked',
-        '95b5ef4e0284f82d4f6e68d750645f3475e174e10a2c33da18e372a212976a8d_unpacked '
+        '95b5ef4e0284f82d4f6e68d750645f3475e174e10a2c33da18e372a212976a8d_unpacked',
+        'fe580d1ff6731875a28c8c9370749aef80cc7ae1cf40d9a656148e00ecf3f5c9'
 ]
 
 
