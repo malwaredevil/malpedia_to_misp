8dd22f450efdb18e876ed497e4a65b2442caad73
Malware Devil <34285973+malwaredevil@users.noreply.github.com>
2021-10-19 23:37:27 +0000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -45,10 +45,6 @@ while [[ $# -gt 0 ]]; do
             _MISP_URL_INPUT="$1";  shift;;
         "-u"| "--malpedia-key" ) 
             _MALPEDIA_KEY_INPUT=$1;  shift;;
-        "0.0.0.0" ) 
-            _POSTGRES_HOST_INPUT="$1";  shift;;
-        "5432" ) 
-            _POSTGRES_PORT_INPUT="$1";  shift;;
         "-r"| "--postgres-user" ) 
             _POSTGRES_USER_INPUT="$1";  shift;;
         "-a"| "--postgres-password") 
